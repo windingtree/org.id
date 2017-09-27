eac4984a741194ab8310a46d64c3af32a105976b
cgewecke <christophergewecke@gmail.com>
2017-09-26 17:20:01 -0700

    Add coverage / parallel travis build

---
+++ .solcover.js
@@ -0,0 +1,12 @@
+module.exports = {
+  norpc: true,
+  copyNodeModules: true,
+  skipFiles: [
+    'lif/FuturePayment.sol',
+    'lif/LifCrowdsale.sol',
+    'lif/LifDAOInterface.sol',
+    'lif/LifInterface.sol',
+    'lif/LifToken.sol',
+    'lif/Migrations.sol'
+  ]
+}
