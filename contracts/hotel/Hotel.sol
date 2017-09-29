c6f0a8edf79a4b73c28ad0cd135d6adf10c59c8e
AugustoL <augusto.lemble@gmail.com>
2017-09-29 12:57:26 +0200

    add comments on Hotel contracts with doxygen style

--- contracts/hotel/Hotel.sol
+++ contracts/hotel/Hotel.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.4.15;
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../Parent.sol";
 
-/*
- * Hotel
- * An indexed contract on the WT Index that contains the hotel information and
- * the addresses of his Unit Types contracts.
+/**
+   @title Hotel, contract for a Hotel registered in the WT network
+
+   A contract that represents a hotels in the WT network, it stores the
+   hotel main information and, location, address, country, timezone, zip,
+   images and the addresses of the hotel unit types and units.
+   Every hotel have different types for units in his inventory and every
+   unit contract save on childs variable reference an unit type.
+   Uses OpenZeppelin Ownable and Parent contract developed by WT.
  */
 contract Hotel is Ownable, Parent {
 
@@ -25,15 +30,20 @@ contract Hotel is Ownable, Parent {
   uint public latitude;
   uint public longitude;
 
-  // The units that the hotel have for rent.
+  // The unit types contracts indexed by type and index.
   mapping(bytes32 => address) public unitTypes;
   bytes32[] public unitTypeNames;
 
   // Hotel images
   string[] public images;
 
-  // Constructor
+  /**
+     @dev Constructor.
 
+     @param _name see `name`
+     @param _description see `description`
+     @param _manager see `_manager`
+   */
   function Hotel(string _name, string _description, address _manager) {
     name = _name;
     description = _description;
@@ -42,8 +52,13 @@ contract Hotel is Ownable, Parent {
     unitTypeNames.length ++;
   }
 
-  // Owner methods
+  /**
+     @dev `editInfo` allows the owner of the contract to change the hotel
+     main information.
 
+     @param _name The new name of the hotel.
+     @param _description The new description of the hotel
+   */
   function editInfo(
     string _name,
     string _description
@@ -52,6 +67,15 @@ contract Hotel is Ownable, Parent {
     description = _description;
   }
 
+  /**
+     @dev `editAddress` allows the owner of the contract to change the hotel
+     phisical address.
+
+     @param _lineOne The new main address of the hotel.
+     @param _lineTwo The new second address of the hotel
+     @param _zip The new zip code of the hotel.
+     @param _country The new country of the hotel
+   */
   function editAddress(
     string _lineOne,
     string _lineTwo,
@@ -64,6 +88,14 @@ contract Hotel is Ownable, Parent {
     country = _country;
   }
 
+  /**
+     @dev `editLocation` allows the owner of the contract to change the hotel
+     location.
+
+     @param _timezone The new timezone of the hotel
+     @param _longitude The new longitude value of the location of the hotel
+     @param _latitude The new longitude value of the latitude of the hotel
+   */
   function editLocation(
     uint _timezone,
     uint _longitude,
@@ -74,6 +106,12 @@ contract Hotel is Ownable, Parent {
     longitude = _longitude;
   }
 
+  /**
+     @dev `addUnitType` allows the owner to add a new unit type.
+
+     @param addr The address of the UnitType contract
+     @param unitType The type of the unit
+   */
   function addUnitType(
     address addr,
     bytes32 unitType
@@ -83,6 +121,12 @@ contract Hotel is Ownable, Parent {
 		unitTypeNames.push(unitType);
 	}
 
+  /**
+     @dev `addUnit` allows the owner to add a new unit in the inventory.
+
+     @param unitType The type of the unit
+     @param unit The address of the Unit contract
+   */
 	function addUnit(
     bytes32 unitType,
     address unit
@@ -91,18 +135,39 @@ contract Hotel is Ownable, Parent {
 		addChild(unit);
 	}
 
+  /**
+     @dev `removeUnit` allows the owner to remove a unit in the inventory.
+
+     @param unit The address of the Unit contract
+   */
   function removeUnit(address unit) onlyOwner() {
 		removeChild(unit);
 	}
 
+  /**
+     @dev `addImage` allows the owner to add an image of the hotel.
+
+     @param url The url of the image
+   */
   function addImage(string url) onlyOwner() {
     images.push(url);
   }
 
+  /**
+     @dev `removeImage` allows the owner to remove an image of the hotel.
+
+     @param index The index of the image in the images array
+   */
   function removeImage(uint index) onlyOwner() {
     delete images[index];
   }
 
+  /**
+     @dev `removeUnitType` allows the owner to remove a unit type.
+
+     @param unitType The type on the unit
+     @param index The index in the unitTypeNames array
+   */
   function removeUnitType(
     bytes32 unitType,
     uint index
@@ -115,6 +180,12 @@ contract Hotel is Ownable, Parent {
     delete unitTypeNames[index];
   }
 
+  /**
+     @dev `changeUnitType` allows the owner to change a unit type.
+
+     @param unitType The type on the unit
+     @param newAddr The new address of the unit type
+   */
   function changeUnitType(
     bytes32 unitType,
     address newAddr
@@ -125,6 +196,12 @@ contract Hotel is Ownable, Parent {
     addChild(newAddr);
   }
 
+  /**
+     @dev `callUnitType` allows the owner to call a unit type.
+
+     @param unitType The type on the unit
+     @param data The data of the call to execute on the unit type contract
+   */
   function callUnitType(
     bytes32 unitType,
     bytes data
@@ -133,6 +210,12 @@ contract Hotel is Ownable, Parent {
     require(unitTypes[unitType].call(data));
   }
 
+  /**
+     @dev `callUnit` allows the owner to call a unit.
+
+     @param unitAddress The address of the unit contract
+     @param data The data of the call to execute on the unit contract
+   */
   function callUnit(
     address unitAddress,
     bytes data
@@ -141,26 +224,51 @@ contract Hotel is Ownable, Parent {
       unitAddress.call(data);
   }
 
-  // Only child methods
+  /**
+     @dev `callIndex` allows a child contract to call the index.
 
+     @param data The data of the call to execute on the index contract
+   */
   function callIndex(bytes data) onlyChild() {
     require(owner.call(data));
   }
 
-  // Public methods
+  /**
+     @dev `getUnitType` get the address of a unit type.
 
+     @param unitType The type of the unit
+
+     @return address Address of the unit type contract
+   */
   function getUnitType(bytes32 unitType) constant returns (address) {
     return unitTypes[unitType];
   }
 
+  /**
+     @dev `getUnitTypeNames` get the names of all the unitTypes.
+
+     @return bytes32[] Names of all the unit types
+   */
   function getUnitTypeNames() constant returns (bytes32[]) {
     return unitTypeNames;
   }
 
+  /**
+     @dev `getImage` get the url of an image.
+
+     @param i The index of the image in the images array
+
+     @return string Url of the image
+   */
   function getImage(uint i) constant returns (string) {
     return images[i];
   }
 
+  /**
+     @dev `getImagesLength` get the length of the images array.
+
+     @return uint Length of the images array
+   */
   function getImagesLength() constant returns (uint) {
     return images.length;
   }
--- contracts/hotel/Unit.sol
+++ contracts/hotel/Unit.sol
@@ -2,40 +2,62 @@ pragma solidity ^0.4.15;
 
 import "../PrivateCall.sol";
 
-/*
- * Unit
- * A type of unit that a Hotel has in his inventory, with all the units
- * information and avaliability.
+ /**
+   @title Unit, contract for a unit in the inventory of a hotel contract
+
+   A contract that represents a hotel unit in the WT network, it stores the
+   unit avaliability and special prices.
+   Uses PrivateCall contract developed by WT.
  */
 contract Unit is PrivateCall {
 
+  // The type of the unit
   bytes32 public unitType;
 
+  // The status of the unit
   bool public active;
+
   // An array of all days avaliability after 01-01-1970
   mapping(uint => UnitDay) reservations;
-
   struct UnitDay {
     string specialPrice;
     address bookedBy;
   }
 
+  /**
+     @dev Event triggered on every booking done
+  **/
   event Book(address from, uint fromDay, uint daysAmount);
 
-  // Constructor
+  /**
+     @dev Constructor. Creates the unit contract in active status
 
+     @param _owner see `owner`
+     @param _unitType see `unitType`
+   */
   function Unit(address _owner, bytes32 _unitType){
     owner = _owner;
     unitType = _unitType;
     active = true;
   }
 
-  // Owner methods
+  /**
+     @dev `setActive` allows the owner of the contract to change the status
 
+     @param _active The new status of the unit
+   */
   function setActive(bool _active) onlyOwner() {
     active = _active;
   }
 
+  /**
+     @dev `setPrice` allows the owner of the contract to set a price for
+     a period of time.
+
+     @param price The price of the unit
+     @param fromDay The starting day of the period of days to change
+     @param daysAmount The amount of days in the period
+   */
   function setPrice(
     string price,
     uint fromDay,
@@ -46,8 +68,15 @@ contract Unit is PrivateCall {
       reservations[i].specialPrice = price;
   }
 
-  // Methods from private call
+  /**
+     @dev `setPrice` allows the contract to execute a book function itself.
 
+     @param from The address of the oener of the reservation
+     @param fromDay The starting day of the period of days to book
+     @param daysAmount The amount of days in the booking period
+     @param finalDataCall A data to execute a call on the hotel contract
+     taht owns this unit
+   */
   function book(
     address from,
     uint fromDay,
@@ -73,8 +102,15 @@ contract Unit is PrivateCall {
     }
   }
 
-  // Public methods
+  /**
+     @dev `getReservation` get the avalibility and price of a day.
 
+     @param day The number of days after 01-01-1970
+
+     @return string The price of the day
+     @return address The address of the owner of the reservation
+     returns 0x0 if its available
+   */
   function getReservation(
     uint day
   ) constant returns(string, address) {
