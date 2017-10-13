97fad4605c19e692e2f006f0bc78b2937a338419
cgewecke <christophergewecke@gmail.com>
2017-10-13 09:56:23 -0700

    Update list of token files to skip for coverage

--- .solcover.js
+++ .solcover.js
@@ -2,13 +2,11 @@ module.exports = {
   norpc: true,
   copyNodeModules: true,
   skipFiles: [
-    'lif/FuturePayment.sol',
     'lif/LifCrowdsale.sol',
-    'lif/LifDAOInterface.sol',
-    'lif/LifInterface.sol',
+    'lif/LifMarketValidationMechanism.sol',
     'lif/LifToken.sol',
     'lif/Migrations.sol',
-    'lif/LifMarketValidationMechanism.sol',
+    'lif/SmartToken.sol',
     'lif/VestedPayment.sol'
   ]
 }
