05aa72ab0ec29a68c58d434129af64ffdd6db8bd
AugustoL <augusto.lemble@gmail.com>
2017-11-03 17:10:10 -0500

    Fix hotel interfaces interfaces

--- contracts/hotel/Hotel_Interface.sol
+++ contracts/hotel/Hotel_Interface.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.4.15;
 
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../PrivateCall.sol";
+import "../Images.sol";
 
 /*
  * Hotel_Interface
  * Interface of Hotel contract
  */
-contract Hotel_Interface is Ownable {
+contract Hotel_Interface is PrivateCall, Images {
 
   // Main information
   string public name;
@@ -23,17 +24,31 @@ contract Hotel_Interface is Ownable {
   uint public latitude;
   uint public longitude;
 
+  // The `UnitType` contracts indexed by type and index
+  mapping(bytes32 => address) public unitTypes;
+  bytes32[] public unitTypeNames;
+
+  // Array of addresses of `Unit` contracts and mapping of their index position
+  mapping(address => uint) public unitsIndex;
+  address[] public units;
+
+  event Book(address from, address unit, uint256 fromDay, uint256 daysAmount);
+
   // Owner methods
   function editInfo(string _name, string _description) onlyOwner();
   function editAddress(string _lineOne, string _lineTwo, string _zip, string _country) onlyOwner() ;
   function editLocation(uint _timezone, uint _longitude, uint _latitude) onlyOwner();
-  function addUnitType(address addr, bytes32 unitType) onlyOwner();
+  function addUnit(address unit) onlyOwner();
+  function removeUnit(address unit) onlyOwner();
+  function addUnitType(address addr) onlyOwner();
   function removeUnitType(bytes32 unitType, uint index) onlyOwner();
   function changeUnitType(bytes32 unitType, address newAddr) onlyOwner();
-  function addImage(string url) onlyOwner();
-  function removeImage(uint index) onlyOwner();
   function callUnitType(bytes32 unitType, bytes data) onlyOwner();
   function callUnit(address unitAddress, bytes data) onlyOwner();
+
+  // Private call methods
+  function book(address unitAddress, address from, uint fromDay, uint daysAmount) fromSelf();
+  function bookWithLif(address unitAddress, address from, uint256 fromDay, uint256 daysAmount) fromSelf();
 
   // Public constant methods
   function getUnitType(bytes32 unitType) constant returns (address);
--- contracts/hotel/Unit_Interface.sol
+++ contracts/hotel/Unit_Interface.sol
@@ -8,15 +8,13 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
  */
 contract Unit_Interface is Ownable {
 
+  // Main Information
   bool public active;
   bytes32 public unitType;
+  uint256 public defaultLifTokenPrice;
 
+  // Events
   event Book(address from, uint fromDay, uint daysAmount);
-
-  // Public methods
-  function getReservation(uint256 day) constant returns(uint256, uint256, address);
-  function getCost(uint256 fromDay, uint256 daysAmount) constant returns(uint256);
-  function getLifCost(uint256 fromDay, uint256 daysAmount) constant returns(uint256);
 
   // Owner methods
   function setActive(bool _active) onlyOwner();
@@ -26,5 +24,10 @@ contract Unit_Interface is Ownable {
   function setDefaultPrice(uint256 price) onlyOwner();
   function setDefaultLifPrice(uint256 price) onlyOwner();
   function book(address from, uint256 fromDay, uint256 daysAmount) onlyOwner() returns(bool);
+
+  // Public methods
+  function getReservation(uint256 day) constant returns(uint256, uint256, address);
+  function getCost(uint256 fromDay, uint256 daysAmount) constant returns(uint256);
+  function getLifCost(uint256 fromDay, uint256 daysAmount) constant returns(uint256);
 
 }
