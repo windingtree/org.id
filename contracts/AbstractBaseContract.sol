5c61b8b3bc647664ba79a0bf4c119a947eae201b
Jirka Chadima <chadima.jiri@gmail.com>
2018-07-26 13:57:13 +0200

    0.2.4

--- contracts/AbstractBaseContract.sol
+++ contracts/AbstractBaseContract.sol
@@ -10,7 +10,7 @@ contract AbstractBaseContract {
 
   // The hex-encoded version, follows the semantic standard MAJOR.MINOR.PATCH-EXTENSION
   // It should always match the version in package.json.
-  bytes32 public version = bytes32("0.2.3");
+  bytes32 public version = bytes32("0.2.4");
 
   // The hex-encoded type of the contract, in all lowercase letters without any spaces.
   // It has to be defined in each contract that uses this interface.
