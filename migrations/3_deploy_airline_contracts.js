fc23385898a1463c44d1f624cdf433cc15cce726
Ondrej Chmelar <ondrej.chmelar@fragaria.cz>
2019-01-09 16:23:37 +0100

    feat(airlines): add a migration to deploy WTAirlinesIndex

--- migrations/3_deploy_airline_contracts.js
+++ migrations/3_deploy_airline_contracts.js
@@ -1,21 +1,20 @@
 const WTAirlineIndex = artifacts.require('WTAirlineIndex');
 
-
 module.exports = function (deployer, network, accounts) {
-  if (network == 'mainnet' || network == 'ropsten') {
+  if (network === 'mainnet' || network === 'ropsten') {
     console.log('Network:', network);
     console.log('Accounts:', accounts);
 
-    const lifTokenAddress = (network == 'mainnet')
+    const lifTokenAddress = (network === 'mainnet')
       ? '0xeb9951021698b42e4399f9cbb6267aa35f82d59d'
       : '0xb6e225194a1c892770c43d4b529841c99b3da1d7';
 
-    return deployer.deploy(WTAirlineIndex).then(function (WTAirlineIndexContract) {
-      console.log('WTAirlineIndex address:', WTAirlineIndexContract.address);
-      return WTAirlineIndexContract.setLifToken(lifTokenAddress);
+    return deployer.deploy(WTAirlineIndex).then(function (wtAirlineIndexContract) {
+      console.log('WTAirlineIndex address:', wtAirlineIndexContract.address);
+      return wtAirlineIndexContract.setLifToken(lifTokenAddress);
     }).then(function (tx) {
-        console.log('LifToken set on tx:', tx.tx);
-        console.log('LifToken address set to:', lifTokenAddress);
+      console.log('LifToken set on tx:', tx.tx);
+      console.log('LifToken address set to:', lifTokenAddress);
     });
   }
 };
