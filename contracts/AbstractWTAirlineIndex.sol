2a39607009e76858f0867842feab630a1818998a
Jirka Chadima <chadima.jiri@gmail.com>
2019-03-14 15:19:42 +0100

    feat: Moves data properties to a single place in WTIndexes

--- contracts/AbstractWTAirlineIndex.sol
+++ contracts/AbstractWTAirlineIndex.sol
@@ -8,12 +8,39 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
  * @dev Interface of WTAirlineIndex contract, inherits from OpenZeppelin's Ownable
  */
 contract AbstractWTAirlineIndex is Ownable {
+    // Array of addresses of `Airline` contracts
     address[] public airlines;
+
+    // Mapping of airlines position in the general airline index
     mapping(address => uint) public airlinesIndex;
+
+    // Mapping of the airlines indexed by manager's address
     mapping(address => address[]) public airlinesByManager;
+    // Mapping of airlines position in the manager's indexed airline index
     mapping(address => uint) public airlinesByManagerIndex;
+
+    // Address of the LifToken contract
     // solhint-disable-next-line var-name-mixedcase
     address public LifToken;
+
+    /**
+     * @dev Event triggered every time airline is registered
+     */
+    event AirlineRegistered(address airline, uint managerIndex, uint allIndex);
+    /**
+     * @dev Event triggered every time airline is deleted
+     */
+    event AirlineDeleted(address airline, uint managerIndex, uint allIndex);
+    /**
+     * @dev Event triggered every time airline is called
+     */
+    event AirlineCalled(address airline);
+
+    /**
+     * @dev Event triggered every time a airline changes a manager.
+     */
+    event AirlineTransferred(address airline, address previousManager, address newManager);
+
 
     function registerAirline(string dataUri) external returns (address);
     function deleteAirline(address airline) external;
@@ -22,9 +49,4 @@ contract AbstractWTAirlineIndex is Ownable {
     function getAirlinesLength() public view returns (uint);
     function getAirlines() public view returns (address[]);
     function getAirlinesByManager(address manager) public view returns (address[]);
-
-    event AirlineRegistered(address airline, uint managerIndex, uint allIndex);
-    event AirlineDeleted(address airline, uint managerIndex, uint allIndex);
-    event AirlineCalled(address airline);
-    event AirlineTransferred(address airline, address previousManager, address newManager);
 }
--- contracts/WTHotelIndex.sol
+++ contracts/WTHotelIndex.sol
@@ -12,39 +12,6 @@ import "./hotel/Hotel.sol";
  */
 contract WTHotelIndex is Initializable, AbstractWTHotelIndex {
 
-    // Array of addresses of `Hotel` contracts
-    address[] public hotels;
-
-    // Mapping of hotels position in the general hotel index
-    mapping(address => uint) public hotelsIndex;
-
-    // Mapping of the hotels indexed by manager's address
-    mapping(address => address[]) public hotelsByManager;
-    // Mapping of hotels position in the manager's indexed hotel index
-    mapping(address => uint) public hotelsByManagerIndex;
-
-    // Address of the LifToken contract
-    // solhint-disable-next-line var-name-mixedcase
-    address public LifToken;
-
-    /**
-     * @dev Event triggered every time hotel is registered
-     */
-    event HotelRegistered(address hotel, uint managerIndex, uint allIndex);
-    /**
-     * @dev Event triggered every time hotel is deleted
-     */
-    event HotelDeleted(address hotel, uint managerIndex, uint allIndex);
-    /**
-     * @dev Event triggered every time hotel is called
-     */
-    event HotelCalled(address hotel);
-
-    /**
-     * @dev Event triggered every time a hotel changes a manager.
-     */
-    event HotelTransferred(address hotel, address previousManager, address newManager);
-
     /**
      * @dev `registerHotel` Register new hotel in the index.
      * Emits `HotelRegistered` on success.
