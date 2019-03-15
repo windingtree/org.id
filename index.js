ba9a042557be2286d9911a3ad22defe5c8b0f565
Jirka Chadima <chadima.jiri@gmail.com>
2019-03-15 11:00:39 +0100

    chore: Omits test contracts from built index.js

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ const AbstractWTAirlineIndexContract = require('./build/contracts/AbstractWTAirl
 const AbstractWTHotelIndexContract = require('./build/contracts/AbstractWTHotelIndex.json');
 const AddressUtilsContract = require('./build/contracts/AddressUtils.json');
 const AirlineContract = require('./build/contracts/Airline.json');
-const AirlineUpgradeabilityTestContract = require('./build/contracts/AirlineUpgradeabilityTest.json');
 const HotelContract = require('./build/contracts/Hotel.json');
-const HotelUpgradeabilityTestContract = require('./build/contracts/HotelUpgradeabilityTest.json');
-const MigrationsContract = require('./build/contracts/Migrations.json');
 const OwnableContract = require('./build/contracts/Ownable.json');
 const WTAirlineIndexContract = require('./build/contracts/WTAirlineIndex.json');
-const WTAirlineIndexUpgradeabilityTestContract = require('./build/contracts/WTAirlineIndexUpgradeabilityTest.json');
 const WTHotelIndexContract = require('./build/contracts/WTHotelIndex.json');
-const WTHotelIndexUpgradeabilityTestContract = require('./build/contracts/WTHotelIndexUpgradeabilityTest.json');
 
 module.exports = {
   AbstractAirlineContract: AbstractAirlineContract,
@@ -22,14 +17,9 @@ module.exports = {
   AbstractWTHotelIndexContract: AbstractWTHotelIndexContract,
   AddressUtilsContract: AddressUtilsContract,
   AirlineContract: AirlineContract,
-  AirlineUpgradeabilityTestContract: AirlineUpgradeabilityTestContract,
   HotelContract: HotelContract,
-  HotelUpgradeabilityTestContract: HotelUpgradeabilityTestContract,
-  MigrationsContract: MigrationsContract,
   OwnableContract: OwnableContract,
   WTAirlineIndexContract: WTAirlineIndexContract,
-  WTAirlineIndexUpgradeabilityTestContract: WTAirlineIndexUpgradeabilityTestContract,
   WTHotelIndexContract: WTHotelIndexContract,
-  WTHotelIndexUpgradeabilityTestContract: WTHotelIndexUpgradeabilityTestContract,
 };
 
