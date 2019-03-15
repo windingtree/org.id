a5deda492184ff97d70469022303e1cf6ad5590d
Jirka Chadima <chadima.jiri@gmail.com>
2019-03-15 14:37:13 +0100

    chore: Replaces custom script with zos bump

--- scripts/set-zos-version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs'),
-  path = require('path'),
-  zosConfig = require('../zos.json'),
-  packageFile = require('../package.json');
-
-zosConfig.version = packageFile.version;
-
-console.log(JSON.stringify(zosConfig, false, '  '));
