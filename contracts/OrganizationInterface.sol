2d6f789d9cf064e7c5da99da228a7ddb6b25157a
Jirka Chadima <chadima.jiri@gmail.com>
2019-06-12 11:15:00 +0200

    docs: Improves OrganizationInterface docs

--- contracts/OrganizationInterface.sol
+++ contracts/OrganizationInterface.sol
@@ -3,11 +3,36 @@ pragma solidity ^0.5.6;
 import "openzeppelin-solidity/contracts/introspection/IERC165.sol";
 
 /**
- * This is an interface every Winding Tree Organization should
- * fullfill.
+ * If you are creating your own implementation of Winding
+ * Tree Organization, this is the minimal interface that you should
+ * fullfill. Without it, the Organization won't be added into the
+ * SegmentDirectory. For checked interface ID, head over to the
+ * implementation of `addOrganization`.
+ *
+ * This is not meant to be used by libraries that try to operate
+ * with the organization as any data manipulation methods are
+ * intentionally omitted.
  */
 contract OrganizationInterface is IERC165 {
+    /**
+     * @dev Returns the address of the current owner.
+     * @return {" ": "Current owner address."}
+     */
     function owner() public view returns (address);
+
+    /**
+     * @dev Returns the URI of ORG.ID JSON file stored off-chain.
+     * @return {" ": "Current ORG.ID JSON URI."}
+     */
     function getDataUri() external view returns (string memory);
+
+    /**
+     * @dev Returns if an `address` is Organization's delegate.
+     * Delegates can operate on behalf of the organization,
+     * typically sign messages.
+     *
+     * @param addr Delegate's Ethereum address
+     * @return {" ": "true if delegate, false otherwise"}
+     */
     function isDelegate(address addr) external view returns(bool);
 }
--- docs/OrganizationInterface.md
+++ docs/OrganizationInterface.md
@@ -24,28 +24,46 @@ Inputs
 
 OrganizationInterface.isDelegate(addr) `view` `07779627`
 
+> Returns if an `address` is Organization's delegate. Delegates can operate on behalf of the organization, typically sign messages.
 
 Inputs
 
 | **type** | **name** | **description** |
 |-|-|-|
-| *address* | addr | undefined |
+| *address* | addr | Delegate's Ethereum address |
 
+Outputs
+
+| **type** | **name** | **description** |
+|-|-|-|
+| *bool* |  | undefined |
 
 ## *function* getDataUri
 
 OrganizationInterface.getDataUri() `view` `65f2c7a7`
 
+> Returns the URI of ORG.ID JSON file stored off-chain.
 
 
 
+Outputs
+
+| **type** | **name** | **description** |
+|-|-|-|
+| *string* |  | undefined |
 
 ## *function* owner
 
 OrganizationInterface.owner() `view` `8da5cb5b`
 
+> Returns the address of the current owner.
 
 
 
+Outputs
+
+| **type** | **name** | **description** |
+|-|-|-|
+| *address* |  | undefined |
 
 ---
