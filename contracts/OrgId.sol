25c5fa18e95ed06fea0f7ad49c0067de1bb2a24a
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-02-26 14:41:12 +0200

    openzeppelin contracts dependency switched to @openzeppelin/contracts package

--- contracts/OrgId.sol
+++ contracts/OrgId.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.5.16;
 
-import "openzeppelin-solidity/contracts/introspection/ERC165.sol";
-import "openzeppelin-solidity/contracts/introspection/ERC165Checker.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/introspection/ERC165.sol";
+import "@openzeppelin/contracts/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "./OrgIdInterface.sol";
 
