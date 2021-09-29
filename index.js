ebdd22fd50e93cf2e8b7d998abc9c4e31fc654bf
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-09-29 11:26:10 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const OrgIdContract = require('./artifacts/contracts/OrgId.sol/OrgId.json');
-const OrgIdContractInterface = require('./artifacts/contracts/IOrgIdRegistry.sol/IOrgIdRegistry.json');
 
 module.exports = {
     OrgIdContract: OrgIdContract,
-    OrgIdAbi: OrgIdContract.abi,
-    OrgIdContractInterface: OrgIdContractInterface
+    OrgIdAbi: OrgIdContract.abi
 };
