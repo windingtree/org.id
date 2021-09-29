07b22bef2aea3da65694f5411e6dcd6f53ce9deb
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-09-29 22:47:28 +0300

    changed package build script

--- index.js
+++
@@ -1,6 +0,0 @@
-const OrgIdContract = require('./artifacts/contracts/OrgId.sol/OrgId.json');
-
-module.exports = {
-    OrgIdContract: OrgIdContract,
-    OrgIdAbi: OrgIdContract.abi
-};
---
+++ index.ts
@@ -0,0 +1,7 @@
+import OrgIdContract from './artifacts/contracts/OrgId.sol/OrgId.json';
+
+export * from './typechain';
+
+export {
+  OrgIdContract
+};
