971818f4107667dfa66543e2e3b59245a56ccc95
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-10-06 15:06:13 +0300

    fixed invalid SPDX id

--- contracts/test/ERC721Receiver.sol
+++ contracts/test/ERC721Receiver.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-only;
+// SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
